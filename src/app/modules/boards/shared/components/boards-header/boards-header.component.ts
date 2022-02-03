b06b01d79ed10bcecc48085c446520186a45a3d7
Joycee Paragas <paragasjoycee@gmail.com>
2022-02-03 21:40:10 +0800

    Created boards module and its corresponding components

---
+++ src/app/modules/boards/shared/components/boards-header/boards-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jp-boards-header',
+  templateUrl: './boards-header.component.html',
+  styleUrls: ['./boards-header.component.scss']
+})
+export class BoardsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
